96a795e7669f1ce6b8d0a1c3c97ca5919514b87e
Senna <sennatitcomb@gmail.com>
2021-04-29 16:57:37 -0700

    Added more test cases

--- calcunittest.py
+++ calcunittest.py
@@ -2,15 +2,30 @@ import unittest
 import inclassactivitycalc
 
 class testCaseCalc(unittest.TestCase):
-    def test_add(self):
-        result = inclassactivitycalc.add(10000000000000000000000000000000000000000,5)
-        result1 = inclassactivitycalc.sub(10,5)
-        result2 = inclassactivitycalc.mul(10,5)
-        result3 = inclassactivitycalc.div(10,5)
-        self.assertEqual(result,10000000000000000000000000000000000000005)
-        self.assertEqual(result1,5)
-        self.assertEqual(result2,50)
-        self.assertEqual(result3,2.0)
+    def test_1(self):
+        result = inclassactivitycalc.add(5,5)
+        self.assertEqual(result,10)
+    def test_2(self):
+       result = inclassactivitycalc.sub(10,5)
+       self.assertEqual(result,5)
+    def test_3(self):
+        result = inclassactivitycalc.mul(12,5)
+        self.assertEqual(result,60)
+    def test_4(self):
+        result = inclassactivitycalc.div(10,5)
+        self.assertEqual(result,2.0)
+    def test_5(self):
+        result = inclassactivitycalc.add(9,5)
+        self.assertEqual(result,3) #fail
+    def test_6(self):
+       result = inclassactivitycalc.sub(8,5)
+       self.assertEqual(result,13) #fail
+    def test_7(self):
+        result = inclassactivitycalc.mul(12,5)
+        self.assertEqual(result,0) #fail
+    def test_8(self):
+        result = inclassactivitycalc.div(10,5)
+        self.assertEqual(result,20) #fail
 
 if __name__ == '__main__':
     unittest.main();
