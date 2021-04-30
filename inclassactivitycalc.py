06c58c0007ab89cd2bf4405699bb488db311e71a
Senna <sennatitcomb@gmail.com>
2021-04-29 21:49:50 -0700

    Part 1 and Part 2

--- inclassactivitycalc.py
+++ inclassactivitycalc.py
@@ -10,3 +10,37 @@ def mul(a,b):
 def div(a,b):
     print(a / b)
     return (a / b)
+
+"""
+import doctest
+def add(a,b):
+    #print(a + b)
+    """
+    >>> add(2, 3)
+    5
+    """
+    return (a + b)
+def sub(a,b):  
+   # print(a - b)
+    """
+    >>> sub(7, 3)
+    4
+    """
+    return (a - b)
+def mul(a,b):
+    #print(a * b)
+    """
+    >>> mul(2, 3)
+    6
+    """
+    return (a * b)
+def div(a,b):
+    #print(a / b)
+    """
+    >>> div(20, 2)
+    10.0
+    """
+    return (a / b)
+doctest.testmod()
+
+"""
